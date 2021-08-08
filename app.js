d88cea42aa2dff0a1bad9220152b7c14e0abdf38
Malte Hübner <maltehuebner@gmx.org>
2021-08-08 13:44:47 +0200

    WIP for fetching critical mass data.

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const fetch = require('node-fetch');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const app = express();
+const port = 3000;
+
+app.get('/:citySlug/:rideIdentifier', (request, response) => {
+    const citySlug = request.params.citySlug;
+    const rideIdentifier = request.params.rideIdentifier;
+
+    const url = 'https://criticalmass.in/api/' + citySlug + '/' + rideIdentifier;
+    const settings = { method: 'Get' };
+
+    fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+            console.log(json);
+            const title = json.title;
+            const dateTime = json.date_time;
+            const location = json.location;
+        });
+    response.send(citySlug + ' ' + rideIdentifier);
 })
 
 app.listen(port, () => {
